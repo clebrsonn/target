5990074a7b5d141b43cf55353e9c410e6b299976
clebrsonn <W0rk5f1n3>
2020-05-06 22:11:48 -0300

    initializing project
    Signed-off-by: clebrsonn <W0rk5f1n3>

---
+++ src/test/java/br/com/hyteck/platform/PlataformaTargetApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.hyteck.platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlataformaTargetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
