8906aedbea99a5806e593af5656607c8b4cba770
clebrsonn <W0rk5f1n3>
2020-05-08 22:34:09 -0300

    preparação para testes
    Signed-off-by: clebrsonn <W0rk5f1n3>

--- src/test/java/br/com/hyteck/platform/PlataformaTargetApplicationTests.java
+++ src/test/java/br/com/hyteck/platform/PlataformaTargetApplicationTests.java
@@ -2,9 +2,11 @@ package br.com.hyteck.platform;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
-class PlataformaTargetApplicationTests {
+@TestPropertySource(locations="classpath:application-test.properties")
+public class PlataformaTargetApplicationTests {
 
     @Test
     void contextLoads() {
